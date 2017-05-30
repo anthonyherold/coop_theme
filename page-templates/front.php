985040ef97022555c9c5fdc0c96962acca9720a4
anthonyherold <herold.anthony@gmail.com>
2017-05-30 18:57:27 +0200

    Avancement 30/05

--- page-templates/front.php
+++ page-templates/front.php
@@ -4,101 +4,53 @@ Template Name: Front
 */
 get_header(); ?>
 
-<header class="front-hero" role="banner">
-	<div class="marketing">
-		<div class="tagline">
-			<h1><?php bloginfo( 'name' ); ?></h1>
-			<h4 class="subheader"><?php bloginfo( 'description' ); ?></h4>
-			<a role="button" class="download large button sites-button hide-for-small-only" href="https://github.com/olefredrik/foundationpress">Download FoundationPress</a>
-		</div>
-
-		<div class="watch">
-			<section class="stargazers">
-				<a href="https://github.com/olefredrik/foundationpress">1.5k stargazers</a>
-			</section>
-			<section class="twitter">
-				<a href="https://twitter.com/olefredrik">@olefredrik</a>
-			</section>
-		</div>
-	</div>
-
-</header>
-
-<?php do_action( 'foundationpress_before_content' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-<section class="intro" role="main">
-	<div class="fp-intro">
-
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php
-					wp_link_pages(
-						array(
-							'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-							'after'  => '</p></nav>',
-						)
-					);
-				?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php do_action( 'foundationpress_page_before_comments' ); ?>
-			<?php comments_template(); ?>
-			<?php do_action( 'foundationpress_page_after_comments' ); ?>
-		</div>
-
-	</div>
-
-</section>
-<?php endwhile;?>
-<?php do_action( 'foundationpress_after_content' ); ?>
-
-<div class="section-divider">
-	<hr />
+<section class="section-1">
+<div class="row">
+		<h3 class="large-6 columns">Qu'est ce que la COOP ?</h3>
+</div>
+<div class="row">
+  <div class="large-7 columns"><img class="img-coop1" src="wp-content/themes/coop_theme/assets/images/photo1.jpg"></div>
+  <div class="large-5 columns"><p><strong>L'Union des Coopérateurs d'Alsace, ou Coop Alsace,</strong> est une entreprise coopérative de distribution, <strong>fondée en 1902 à Strasbourg</strong> sous le nom de Konsumverein für Strassburg und Umgegend. <br /><br />Le Port du Rhin à Strasbourg héberge depuis 1911 les locaux du siège de la Coop.<br /><br />Peu à peu, le bâtiment initial à Strasbourg se voit rejoint par différents bâtiments industriels avec des fonctions précises : entrepôts pour la mercerie, l'emballage, la boucherie-charcuterie ou l'électroménager, chai d'embouteillage et de stockage, garage...<br /><br /><strong>Depuis 2009 la coop s’est associé avec le grand groupe E.LECLERC</strong> après des difficultés financières. Petit à petit les locaux de la coop sont abandonnés par LECLERC qui béneficie d’une chaine de distribution nationale.</p></div>
 </div>
 
-
-<section class="benefits">
-	<header>
-		<h2>Build Foundation based sites, powered by WordPress</h2>
-		<h4>Foundation is the professional choice for designers, developers and teams. <br /> WordPress is by far, <a href="http://trends.builtwith.com/cms">the world's most popular CMS</a> (currently powering 38% of the web).</h4>
-	</header>
-
-	<div class="semantic">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/semantic.svg" alt="semantic">
-		<h3>Semantic</h3>
-		<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
-	</div>
-
-	<div class="responsive">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/responsive.svg" alt="responsive">
-		<h3>Responsive</h3>
-		<p>You can build for small devices first. Then, as devices get larger and larger, layer in more complexity for a complete responsive design.</p>
-
-	</div>
-
-	<div class="customizable">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/customizable.svg" alt="customizable">
-		<h3>Customizable</h3>
-		<p>You can customize your build to include or remove certain elements, as well as define the size of columns, colors, font size and more.</p>
-
-	</div>
-
-	<div class="professional">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/professional.svg" alt="professional">
-		<h3>Professional</h3>
-		<p>Millions of designers and developers depend on Foundation. We have business support, training and consulting to help grow your product or service.</p>
-	</div>
-
-	<div class="why-foundation">
-		<a href="/kitchen-sink">See what's in Foundation out of the box →</a>
-	</div>
-
+<div class="row">
+  <div class="large-6 columns"><p>Après la cessation d’activité, la coop est racheté par la Société Publique Locale DeuxRives en 2015.<br /><br />
+A travers les orientations du Plan-Guide, <strong>la Coop vise à offrir des espaces et une ville de nouvelles mixités et coopérations.</strong> <br /><br />
+Aux côtés des espaces à vocation socio-culturelle, de l’habitat et des bureaux, la vocation productive est cultivée et vise à s’inscrire dans la dynamique créative et sociale du site : la menuiserie de la coop peut accueillir des ateliers de fabrication et d’expérimentation ouvertes, les activités logistique des transports Chalot sont maintenues sur le site, <strong>la production agro-alimentaire historique de la Coop Alsace peut se réinventer autour de projets de consommation responsable et de circuits courts.</strong> </p></div>
+  <div class="large-6 columns"><img class="img-coop2" src="wp-content/themes/coop_theme/assets/images/photo2.jpg"></div>
+</div>
 </section>
 
 
+<section class="section-2">
+	<div class="row">
+			<h3 class="large-6 columns">Le projet de la virgule</h3>
+		</div>
+			<img class="la-virgule" src="wp-content/themes/coop_theme/assets/images/virgule.jpg">
+			<div class="row"><p>A l’ouest du site, face à la Capitainerie, se situaient les anciens garages et ateliers de la Coopé. Ils composent, avec les ateliers du Port Autonome de Strasbourg au Sud, <strong>la forme caractéristique de « virgule ».</strong><br /><br />
+Les bâtiments et la vaste cour centrale offrent des espaces privilégiés pour une appropriation <strong>artistique</strong> et </strong>créative</strong> des lieux.<br /><br />
+C’est sur ce site que la Ville de Strasbourg et la SPL Deux-Rives visent à consolider un lieu de vie et d’échanges structurant du site COOP, tirant parti de la configuration des espaces extérieurs et des qualités des bâtiments existants, <strong>tout en développant une relation forte avec les autres acteurs et espaces du projet</strong> (ex : espaces polyvalents de la cave à vins, acteurs du projets kaléidoscoop, futurs acteurs du site et habitants ou travailleurs du quartier des deuxrives/Port du Rhin, etc).</p>
+		<h4>Composition de la virgule</h4>
+		</div>
+		<div class="row">
+	  <div class="small-12 large-4 columns"><img class="menuiserie" src="wp-content/themes/coop_theme/assets/images/menuiserie.jpg"></div>
+	  <div class="small-12 large-4 columns"><img class="semencerie" src="wp-content/themes/coop_theme/assets/images/semencerie.jpg"></div>
+	  <div class="small-12 large-4 columns"><img class="garage" src="wp-content/themes/coop_theme/assets/images/garage.jpg"></div>
+	</div>
+</section>
 
-<?php get_footer();
+<section class="section-3 text-center">
+	<div class="row">
+	<h5>N’HESITEZ PLUS A REJOINDRE <br />
+		<strong>UN PROJET INNOVANT</strong> ET <strong> DEVENEZ <br />
+		INVESTISEURS DE LA COOPÉRATRIVE CRÉATIVE</strong></h5>
+		<a href="about.html" class="button">REJOINDRE LE PROJET</a>
+	</div>
+	</section>
+
+
+<?php get_footer();?>
+
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.12'><\/script>".replace("HOST", location.hostname));
+//]]></script>
